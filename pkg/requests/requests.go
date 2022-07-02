bac46372a3c87975ca574ade8893f8dec915e37a
u1ug <maxim2006722@gmail.com>
2022-07-02 13:14:20 +0300

    go http request wrapper

--- pkg/requests/requests.go
+++ pkg/requests/requests.go
@@ -1,1 +1,33 @@
 package requests
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
+
+func Post(jsonData []byte, addr string) (error, []byte) {
+	resp, err := http.Post(addr, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err, nil
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err, nil
+	}
+	return err, body
+}
+
+func Get(addr string) (error, []byte) {
+	resp, err := http.Get(addr)
+	if err != nil {
+		return err, nil
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err, nil
+	}
+	return err, body
+}
